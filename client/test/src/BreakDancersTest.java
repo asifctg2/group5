0c96edb82c00c5fb7f67ec0de5b8cb9b17103a48
Andy Vargas <94484667+andyvn1@users.noreply.github.com>
2022-07-27 14:21:57 -0700

    testing classes

---
+++ client/test/src/BreakDancersTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BreakDancersTest {
+
+
+
+    @Test
+    void dance() {
+        Character soulStepper = new Player("Daniel", 100);
+        Character enemy = new BreakDancers("enemyTest", 100);
+
+        enemy.dance(soulStepper, enemy);
+    }
+}
---
+++ client/test/src/ItemsTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemsTest {
+
+    @Test
+    void useItem() {
+        Location test = new Location("test");
+        Player player = new Player("Andy", 20);
+        Items items = new Items();
+        test.addItem("health kit");
+        System.out.println("Before using health kit, health = " + player.getHealth());
+        items.useItem("health kit", player);
+        boolean increase = player.getHealth() >= 20;
+        Assert.assertEquals(increase, true);
+        System.out.println(player.getHealth());
+
+
+    }
+}
